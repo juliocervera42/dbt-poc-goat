a4fd142ddb3741a6570cfb23413cb3bf91c96ca0
juliocervera42 <juliocervera42@gmail.com>
2025-08-04 13:19:16 -0600

    actividad principal por entidad

--- models/example/denue_1.sql
+++ models/example/denue_1.sql
@@ -1,5 +1,14 @@
-select
-id
-from
-denue_upload
-limit 10
+select 
+entidad, 
+codigo_act,
+total
+from (
+    select entidad,
+    codigo_act,
+    count(*) as total
+    row_number() over (partition by entidad order by  count(*) desc) as rn
+    from denue_upload
+    group by entidad, codigo_act
+) sub
+where rn = 1
+order by entidad
